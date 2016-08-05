aa763cb0a40d52a5a3f2ca3cda5d817579219208
Tomas Brambora <tomas.brambora@gmail.com>
2016-08-06 09:17:42 +1000

    WIP

--- imports/ui/pages/shareCompost/shareCompost.jsx
+++ imports/ui/pages/shareCompost/shareCompost.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Geosuggest from 'react-geosuggest';
+import './index.less';
 
 class ShareCompostPage extends React.Component {
+
+  onSuggestSelect() {
+    console.log(arguments);
+  }
+
   render() {
     return (
-      <div>
+      <div className="share-compost-root">
         Share compost
+        <Geosuggest
+          placeholder="Type your address"
+          onSuggestSelect={this.onSuggestSelect}
+          />
       </div>
     )
   }
